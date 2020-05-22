429b044e97f48a6f077d28aac9f6670c246bd317
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-05-23 00:12:21 +0500

    Object pooler implemented to optimize the game

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string name;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static ObjectPooler objectPoolerInstance;
+
+    private void Awake()
+    {
+        objectPoolerInstance = this;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        InstantiatePoolObjects();
+    }
+
+    private void InstantiatePoolObjects()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.name, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string name, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(name))
+        {
+            Debug.Log("This name " + name + " does not excist");
+            return null;
+        }
+        
+        GameObject objectToSpawn = poolDictionary[name].Dequeue();
+        if (objectToSpawn.activeSelf)
+        {
+            InstantiatePoolObjects();
+            
+            objectToSpawn = poolDictionary[name].Dequeue();
+        }
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[name].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
