e94a09f4b5009d967d1db93fd4c777e504c4c4e1
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-01 21:10:15 +0500

    Bug fixes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 0;
-    [SerializeField] Rigidbody2D rb = null;
+    [SerializeField] private Rigidbody2D rb = null;
 
     //Object Pooler
-    ObjectPooler objectPooler;
+    BulletExplosionPooler bulletExplosionPooler;
 
     private void Start()
     {
         //Play sound
         FindObjectOfType<AudioManager>().Play("PlayerBullet");
 
-        objectPooler = ObjectPooler.objectPoolerInstance;
+        bulletExplosionPooler = BulletExplosionPooler._instance;
     }
 
     private void FixedUpdate()
@@ -50,22 +50,21 @@ public class Bullet : MonoBehaviour
     {
         //Depending on bullet instantiate corresponding particles
         if(gameObject.name == "PlayerBullet(Clone)")
-        {            
-            objectPooler.SpawnFromPool("PlayerBulletExplosion", transform.position, Quaternion.identity);
+        {
+            bulletExplosionPooler.SpawnFromPool("PlayerBulletExplosion", transform.position, Quaternion.identity);
         }
         else if(gameObject.name == "EnemyBullet(Clone)")
-        {         
-            objectPooler.SpawnFromPool("LittleEnemyBulletExplosion", transform.position, Quaternion.identity);
+        {
+            bulletExplosionPooler.SpawnFromPool("LittleEnemyBulletExplosion", transform.position, Quaternion.identity);
         }
         else if(gameObject.name == "LittleEnemyBullet(Clone)")
-        {         
-            objectPooler.SpawnFromPool("AverageEnemyBulletExplosion", transform.position, Quaternion.identity);
+        {
+            bulletExplosionPooler.SpawnFromPool("AverageEnemyBulletExplosion", transform.position, Quaternion.identity);
         }
         else if(gameObject.name == "MegaEnemyBullet(Clone)")
-        {         
-            objectPooler.SpawnFromPool("MegaEnemyBulletExplosion", transform.position, Quaternion.identity);
+        {
+            bulletExplosionPooler.SpawnFromPool("MegaEnemyBulletExplosion", transform.position, Quaternion.identity);
         }
-        //Destroy(gameObject);
         gameObject.SetActive(false);
     }
 }
