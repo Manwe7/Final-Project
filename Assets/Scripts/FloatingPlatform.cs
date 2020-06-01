4ac57ba583b6f0a23538038b05d01c711cdf5187
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-01 16:23:01 +0500

    Floating platform script (moves up and down)

---
+++ Assets/Scripts/FloatingPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPlatform : MonoBehaviour
+{
+    private Vector2 _pointA, _pointB;
+
+    void Start()
+    {
+        _pointA = new Vector2(transform.position.x, transform.position.y + 4f);
+        _pointB = new Vector2(transform.position.x, transform.position.y - 4f);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(_pointA, _pointB, Mathf.PingPong(Time.time / 2, 1));
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
