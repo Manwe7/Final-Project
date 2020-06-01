ba3270fae60007e3772e1a2b5be30eae407a4c11
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-02 00:52:29 +0500

    Use new pool system

--- Assets/Scripts/Enemy/LittleEnemy.cs
+++ Assets/Scripts/Enemy/LittleEnemy.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class LittleEnemy : Enemy
 {
     [Header("Buttet and barrel")]
-    //[SerializeField] private Transform bullet = null;
     [SerializeField] private Transform barrel = null;
 
     private int _reloadTime;
     private bool _reloaded;
 
     //Object Pooler
-    ObjectPooler objectPooler;
+    ExplosionPooler explosionPooler;
+    BulletPooler bulletPooler;
 
     private void Start()
     {
@@ -19,7 +19,8 @@ public class LittleEnemy : Enemy
 
         StartCoroutine(Reload());
 
-        objectPooler = ObjectPooler.objectPoolerInstance;
+        explosionPooler = ExplosionPooler._instance;
+        bulletPooler = BulletPooler._instance;
     }
 
     private void Update()
@@ -27,13 +28,13 @@ public class LittleEnemy : Enemy
         if (_health <= 0)
         {
             GameManager.gameManagerInstance.CurrentScore += 5;
-            objectPooler.SpawnFromPool("LittleEnemyExplosion", transform.position, Quaternion.identity);
+            explosionPooler.SpawnFromPool("LittleEnemyExplosion", transform.position, Quaternion.identity);
             gameObject.SetActive(false);
         }
 
         if (_reloaded)
         {
-            objectPooler.SpawnFromPool("LittleEnemyBullet", barrel.transform.position, barrel.transform.rotation);
+            bulletPooler.SpawnFromPool("LittleEnemyBullet", barrel.transform.position, barrel.transform.rotation);
             //Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
             StartCoroutine(Reload());
         }
