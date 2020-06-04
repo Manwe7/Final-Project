2a3340f5ddd109c4543ca6e5bc72fc00e1ef87f2
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-04 23:25:05 +0500

    Player Setup to separate controlls

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] private PlayerOnline playerOnline = null;
+    [SerializeField] private PlayerOnlineMovement playerOnlineMovement = null;
+    [SerializeField] private GameObject cameraPlayer = null;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            playerOnline.enabled = false;
+            playerOnlineMovement.enabled = false;
+            cameraPlayer.SetActive(false);
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
