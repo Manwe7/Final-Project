844e21c9b0e6cfbe8e0608fc182efea46553617e
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-06 00:25:07 +0500

    Change layer of player

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,17 +5,26 @@ public class PlayerSetup : NetworkBehaviour
 {
     [SerializeField] private PlayerOnline playerOnline = null;
     [SerializeField] private PlayerOnlineMovement playerOnlineMovement = null;
+    [SerializeField] private PlayerOnlineWeapon playerOnlineWeapon = null;
     [SerializeField] private GameObject cameraPlayer = null;
+    [SerializeField] private string remoteLayerName = "RemotePlayer";
 
     Camera sceneCamera;
 
     private void Start()
     {
+        string _ID = "Player" + GetComponent<NetworkIdentity>().netId;
+
+        gameObject.name = _ID;
+
         if (!isLocalPlayer)
         {
             playerOnline.enabled = false;
             playerOnlineMovement.enabled = false;
+            playerOnlineWeapon.enabled = false;
             cameraPlayer.SetActive(false);
+
+            gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
         }
         else
         {
@@ -25,7 +34,7 @@ public class PlayerSetup : NetworkBehaviour
                 sceneCamera.gameObject.SetActive(false);
             }
         }
-    }
+    }   
 
     private void OnDisable()
     {
