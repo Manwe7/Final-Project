f74aa7b8d0486ea32da6c396dbea7680a0c2b206
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-15 21:54:42 +0500

    Photon added

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Text LogText = null;
+
+    private void Start()
+    {
+        PhotonNetwork.NickName = "Player " + Random.Range(100, 999);
+        Log("Player name is " + PhotonNetwork.NickName);
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+
+    public void RandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Connected");
+
+        PhotonNetwork.LoadLevel("GameOnline");
+    }
+
+    private void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
+    }
+}
