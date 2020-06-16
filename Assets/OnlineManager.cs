0a3bac04dfd949d91de0d3764582ef8f2a64a5ea
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-16 21:42:05 +0500

    Online manager for Online Game scene

---
+++ Assets/OnlineManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class OnlineManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject Player = null;
+
+    private void Start()
+    {
+        PhotonNetwork.Instantiate(Player.name, Vector3.zero, Quaternion.identity);
+    }
+
+    public void LeaveRoom()
+    { 
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        Debug.Log("Player " + newPlayer + " enter the game");
+    }
+
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        Debug.Log("Player " + otherPlayer + " left the game");
+    }
+}
