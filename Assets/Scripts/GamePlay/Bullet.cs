d311ad5c28ecb4e740f6a6a61f583024761c9071
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-24 14:33:06 +0500

    Bullet fixes after code review

--- Assets/Scripts/GamePlay/Bullet.cs
+++ Assets/Scripts/GamePlay/Bullet.cs
@@ -5,6 +5,8 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float speed = 0;
     [SerializeField] private Rigidbody2D _rigidbody2D = null;
 
+    private string _explosionName;
+
     //Object Pooler
     Pooler pooler;
 
@@ -12,6 +14,8 @@ public class Bullet : MonoBehaviour
     {
         //Play sound
         FindObjectOfType<AudioManager>().Play("PlayerBullet");
+
+        _explosionName = gameObject.name + "Explosion";
 
         pooler = Pooler.Instance;
     }
@@ -48,39 +52,11 @@ public class Bullet : MonoBehaviour
 
     void Explode()
     {
-        //tones of if by name....
-        //1 - better create a property (type Enum) with predefined values
-        //2 - even better solution - use strategy pattern and inject proper explosion type whe you initialize the object
-        //3 - the logic in if is 75% the same. the difference is only which object you got from pool, this requires refactoring
-        //Depending on bullet instantiate corresponding particles
-        if(gameObject.name == "PlayerBullet")
-        {
-            GameObject explosion = pooler.GetPooledObject("PlayerBulletExplosion");
-            explosion.transform.position = transform.position;
-            explosion.transform.rotation = Quaternion.identity;
-            explosion.SetActive(true);
-        }
-        else if(gameObject.name == "LittleEnemyBullet")
-        {
-            GameObject explosion = pooler.GetPooledObject("LittleEnemyBulletExplosion");
-            explosion.transform.position = transform.position;
-            explosion.transform.rotation = Quaternion.identity;
-            explosion.SetActive(true);
-        }
-        else if(gameObject.name == "AverageEnemyBullet")
-        {
-            GameObject explosion = pooler.GetPooledObject("AverageEnemyBulletExplosion");
-            explosion.transform.position = transform.position;
-            explosion.transform.rotation = Quaternion.identity;
-            explosion.SetActive(true);
-        }
-        else if(gameObject.name == "MegaEnemyBullet")
-        {
-            GameObject explosion = pooler.GetPooledObject("MegaEnemyBulletExplosion");
-            explosion.transform.position = transform.position;
-            explosion.transform.rotation = Quaternion.identity;
-            explosion.SetActive(true);
-        }
+        GameObject explosion = pooler.GetPooledObject(_explosionName);
+        explosion.transform.position = transform.position;
+        explosion.transform.rotation = Quaternion.identity;
+        explosion.SetActive(true);
+
         gameObject.SetActive(false);
     }
 }
