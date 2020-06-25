f0f963b2606df86ff325f7d0ad4f16921ab08335
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-06-25 12:16:45 +0500

    Bug fixes with pause button

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -14,9 +14,7 @@ public class UIManager : MonoBehaviour
     {
         Player.defeated -= Defeat;
     }
-    //not clear name of function
-    //I assume that it is used for two buttons (as there are 2 if inside
-    //separate the logic in separate functions for separate buttons
+    
     public void PauseBtn()
     {
         if (!PauseMenuPanel.activeSelf)
@@ -24,27 +22,10 @@ public class UIManager : MonoBehaviour
             PauseMenuPanel.SetActive(true);
             Time.timeScale = 0f;
         }
-        else if (PauseMenuPanel.activeSelf)//it is redundant
-        {
-            PauseMenuPanel.SetActive(false);
-            Time.timeScale = 1f;
-        }
     }
-
-    /*private void Update()
-    {
-        if (GameManager.gameManagerInstance._player.activeSelf == false)
-        {
-            GameManager.gameManagerInstance.SaveRecord();
-            Time.timeScale = 0.5f;
-            Invoke("OpenDefeatMenu", 1.5f);
-        }
-    }*/
 
     private void Defeat()
     {
-        //may be subscribe gamemanager separatelly to event in Player?
-        GameManager.gameManagerInstance.SaveRecord();
         Time.timeScale = 0.5f;
         Invoke("OpenDefeatMenu", 1.5f);
     }
@@ -52,7 +33,6 @@ public class UIManager : MonoBehaviour
     private void OpenDefeatMenu()
     {
         //Open defeat menu
-        GameManager.gameManagerInstance.SaveRecord();
         DefeatMenuPanel.SetActive(true);
         Time.timeScale = 0f;
     }
