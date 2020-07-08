003c31b0e5ee61a713988af72abcaf5e497c82f6
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-08 21:32:38 +0500

    Code fixes (part 4)

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,30 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
-	public float ShakeDuration = 0.3f;          // Time the Camera Shake effect will last
-    public float ShakeAmplitude = 1.2f;         // Cinemachine Noise Profile Parameter
-    public float ShakeFrequency = 2.0f;         // Cinemachine Noise Profile Parameter
-    private float ShakeElapsedTime = 0f;
-    public static bool ShakeOnce = false;
+	[SerializeField] private float ShakeDuration = 0.3f;  // Time the Camera Shake effect will last
+    [SerializeField] private float ShakeAmplitude = 1.2f; // Cinemachine Noise Profile Parameter
+    [SerializeField] private float ShakeFrequency = 2.0f; // Cinemachine Noise Profile Parameter
 
-    [SerializeField]private CinemachineVirtualCamera VirtualCamera = null;
+    [SerializeField] private CinemachineVirtualCamera VirtualCamera = null;
+
     private CinemachineBasicMultiChannelPerlin virtualCameraNoise = null;
-
-
-	void Start()
+    private float ShakeElapsedTime = 0f;
+    
+    public static bool ShakeOnce = false;
+    
+	private void Start()
     {
         // Get Virtual Camera Noise Profile
         if (VirtualCamera != null)
+        {
             virtualCameraNoise = VirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+        }
     }
 
-	void Update()
+	private void Update()
     {
-        // TODO: Replace with your trigger
         if (ShakeOnce == true)
         {
             ShakeElapsedTime = ShakeDuration;
