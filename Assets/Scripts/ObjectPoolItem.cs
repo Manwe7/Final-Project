54044a624d14aff1c50acd78529ef2820cdab62e
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-08 18:15:20 +0500

    New object pool type (does not work yet)

---
+++ Assets/Scripts/ObjectPoolItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ObjectPoolItem
+{
+    public enum ObjectType
+    {
+        PlayerBullet,
+        LittleEnemyBullet,
+        AverageEnemyBullet,
+        MegaEnemyBullet,
+        PlayerBulletExplosion,
+        LittleEnemyBulletExplosion,
+        AverageEnemyBulletExplosion,
+        MegaEnemyBulletExplosion,
+        PlayerExplosion, 
+        LittleEnemyExplosion,
+        AverageEnemyExplosion,
+        MegaEnemyExplosion,
+        LittleEnemy,
+        AverageEnemy,
+        MegaEnemy
+    }
+    public ObjectType objectType;
+
+    public GameObject objectToPool;
+    public int amountToPool;
+}
