d6018dccfa797122fc005a50ddd05e71c1a5b2f7
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-09 14:12:23 +0500

    Code fixes (4)

--- Assets/Scripts/GamePlay/FloatingPlatform.cs
+++ Assets/Scripts/GamePlay/FloatingPlatform.cs
@@ -4,18 +4,18 @@ public class FloatingPlatform : MonoBehaviour
 {
     private Vector2 _pointA, _pointB;
 
-    void Start()
+    private void Start()
     {
         _pointA = new Vector2(transform.position.x, transform.position.y + 4f);
         _pointB = new Vector2(transform.position.x, transform.position.y - 4f);
     }
 
-    void Update()
+    private void Update()
     {
         transform.position = Vector3.Lerp(_pointA, _pointB, Mathf.PingPong(Time.time / 2, 1));
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -23,7 +23,7 @@ public class FloatingPlatform : MonoBehaviour
         }
     }
 
-    void OnCollisionExit2D(Collision2D other)
+    private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
