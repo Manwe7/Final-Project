0608284ee78fb923368209bbb1f9be6d944266f6
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-10 16:01:54 +0500

    Code fixes (part 4)

--- Assets/Scripts/Pooler.cs
+++ Assets/Scripts/Pooler.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 
 public class Pooler : MonoBehaviour
 {
-    [SerializeField] private Transform Container = null;
-
     [System.Serializable]
-    public class ObjectPoolItem
+    public class GameObjectToPool
     {
         public string objectName;
         public GameObject objectToPool;
@@ -14,20 +12,32 @@ public class Pooler : MonoBehaviour
         public bool shouldExpand;
     }
 
+
+    [SerializeField] private Transform Container = null;    
+
     public static Pooler Instance;
-    public List<ObjectPoolItem> itemsToPool;
+    public List<GameObjectToPool> itemsToPool;
     private List<GameObject> pooledObjects;
 
     void Awake()
     {
-        Instance = this;
+        #region Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
     }
 
     // Use this for initialization
     void Start()
     {       
         pooledObjects = new List<GameObject>();
-        foreach (ObjectPoolItem item in itemsToPool)
+        foreach (GameObjectToPool item in itemsToPool)
         {
             for (int i = 0; i < item.amountToPool; i++)
             {
@@ -40,27 +50,32 @@ public class Pooler : MonoBehaviour
         }
     }
 
-    public GameObject GetPooledObject(string name)
+    public GameObject GetPooledObject(string name, Vector3 _position, Quaternion _rotation)
     {
         for (int i = 0; i < pooledObjects.Count; i++)
         {
             if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].name == name)
             {
+                pooledObjects[i].transform.position = _position;
+                pooledObjects[i].transform.rotation = _rotation;
+                pooledObjects[i].SetActive(true);
                 return pooledObjects[i];
             }
         }
-        foreach (ObjectPoolItem item in itemsToPool)
+        foreach (GameObjectToPool item in itemsToPool)
         {
             if (item.objectToPool.name == name)
             {
                 if (item.shouldExpand)
                 {
-                    GameObject obj = Instantiate(item.objectToPool);
-                    obj.transform.parent = Container;
-                    obj.name = item.objectName;
-                    obj.SetActive(false);
-                    pooledObjects.Add(obj);
-                    return obj;
+                    GameObject gameObject = Instantiate(item.objectToPool);
+                    gameObject.transform.parent = Container;
+                    gameObject.name = item.objectName;
+                    gameObject.transform.position = _position;
+                    gameObject.transform.rotation = _rotation;
+                    gameObject.SetActive(true);
+                    pooledObjects.Add(gameObject);
+                    //return obj;
                 }
             }
         }
