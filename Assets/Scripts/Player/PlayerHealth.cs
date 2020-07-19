b85608c2dc1bd8ad983e48ac14492d01b2926aa6
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-19 22:04:16 +0500

    New script to handle player Health

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private Slider _healthSlider;
+    private int _health;
+
+    public event Action IsKilled;
+
+    private void Start()
+    {
+        _health = 100;
+        _healthSlider.maxValue = _health;
+        _healthSlider.value = _health;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+        _healthSlider.value = _health;
+    } 
+
+    public void Killed()
+    {        
+        _health = 0;
+        _healthSlider.value = _health;
+        IsKilled();
+    }    
+}
