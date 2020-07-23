036c94afb8f5c25bb4b98f34c8e4324ebc938d1f
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-23 16:57:19 +0500

    To control game speed

---
+++ Assets/Scripts/GamePlay/GameSpeed.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameSpeed : MonoBehaviour
+{
+    public void ToNormal()
+    {
+        SetTimeScale(1f);
+    }
+
+    public void ToHalfSpeed()
+    {
+        SetTimeScale(0.5f);
+    }
+
+    public void Stop()
+    {
+        SetTimeScale(0f);
+    }
+
+    private void SetTimeScale(float value)
+    {
+        Time.timeScale = value;
+    }
+}
