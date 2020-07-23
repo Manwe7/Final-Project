a522f52db89f54567a02b0d559de854b9afaa678
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-23 17:34:24 +0500

    Rename files

--- Assets/Scripts/StaticStringNames.cs
+++
@@ -1,7 +0,0 @@
-﻿public class StaticStringNames
-{
-    public static readonly string Menu = "Menu";
-    public static readonly string Game = "Game";
-    public static readonly string Lobby = "Lobby";
-    public static readonly string GameOnline = "GameOnline";
-}
