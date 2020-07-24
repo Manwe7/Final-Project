84924ca93e4c59d4bf9108e79b6382b9b1134ab4
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-24 19:28:06 +0500

    To save data

---
+++ Assets/Scripts/Managers/SaveSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour
+{
+    private const string record = "Record";
+
+    public void SaveRecord(int value)
+    {
+        PlayerPrefs.SetInt("Record", value);
+    }
+}
