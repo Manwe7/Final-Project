cfb3d57e82dcf6d6cead8be4ced15451e285ddf7
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-24 19:28:45 +0500

    To handle Fuel logic

---
+++ Assets/Scripts/Player/FuelHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelHandler : MonoBehaviour
+{
+    [SerializeField] private Slider _playerFuelSlider;
+    
+    [SerializeField] private PlayerFlying _playerFlying;
+
+    [HideInInspector] public float _maxfuelCapacity = 50;
+
+    [HideInInspector] public float _fuelCapacity;
+
+    private void Start()
+    {
+        _playerFlying.OnFuelCapacityChange += SliderValue;
+
+        _fuelCapacity = _maxfuelCapacity;
+        _playerFuelSlider.maxValue = _maxfuelCapacity;
+    }
+
+    private void OnDestroy()
+    {
+        _playerFlying.OnFuelCapacityChange -= SliderValue;
+    }
+
+    private void SliderValue(float capacity)
+    {
+        _playerFuelSlider.value = _fuelCapacity;
+    }
+}
