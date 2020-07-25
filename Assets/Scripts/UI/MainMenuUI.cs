5b1876afd2681e52960637a583622d261075e11e
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-25 17:49:57 +0500

    Bug fixes

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,7 +5,7 @@ public class MainMenuUI : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneNames.Menu);
+        SceneManager.LoadScene(SceneNames.Game);
     }
 
     public void Lobby()
