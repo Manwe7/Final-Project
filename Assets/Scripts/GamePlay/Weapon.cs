cf02e4160a149b7ac9bc1bb8b8d7da7efeedbd4e
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-25 17:54:27 +0500

    Weapon base class for Player and Enemy weapons

---
+++ Assets/Scripts/GamePlay/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    [SerializeField] protected Transform _parent;
+
+    protected float _rotationZ;
+    protected Vector3 _direction;
+    protected float _offset;
+
+    protected void Update() 
+    {
+        ChangeWeaponPosition();
+    }
+
+    protected abstract void ChangeWeaponPosition();
+}
