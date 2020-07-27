13f4bea5ca5b4e9e746157d7f771d5744680023b
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-27 23:34:31 +0500

    Spawn in random positions

--- Assets/Scripts/Online/OnlineManager.cs
+++ Assets/Scripts/Online/OnlineManager.cs
@@ -3,13 +3,42 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 
 public class OnlineManager : MonoBehaviourPunCallbacks
-{
-    [SerializeField] private GameObject Player;
+{    
+    [SerializeField] private MapSections _mapSections;
+ 
+    [SerializeField] private GameObject _player;
+
+    [SerializeField] private GameObject _pausePanel;
+
+    private void Awake()
+    {
+        // if(PhotonNetwork.IsMasterClient)
+        // {
+        //     _mapSections.enabled = true;
+        // }
+        // else
+        // {
+        //     _mapSections.enabled = false;
+        // }
+    }
 
     private void Start()
     {
-        GameObject player = PhotonNetwork.Instantiate(Player.name, Vector3.zero, Quaternion.identity);
+        int posX = Random.Range(-55, 55);
+        int posy = Random.Range(-7, 25);
+
+        GameObject player = PhotonNetwork.Instantiate(_player.name, new Vector2(posX, posy), Quaternion.identity);
         player.name = "Player" + PhotonNetwork.NickName;
+    }
+
+    public void PauseGame()
+    {
+        _pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        _pausePanel.SetActive(false);
     }
 
     public void LeaveRoom()
