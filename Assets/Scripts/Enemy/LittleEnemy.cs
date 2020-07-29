b352f5d80a7165ee593339d176540c223da788fe
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-29 17:15:50 +0500

    SOLID implementation

--- Assets/Scripts/Enemy/LittleEnemy.cs
+++ Assets/Scripts/Enemy/LittleEnemy.cs
@@ -3,8 +3,10 @@
 public class LittleEnemy : Enemy
 {
     protected override int _scoreWeight => 5;
+
     private void OnEnable()
     {
         _health = Random.Range(10, 20);
+        _distance = Random.Range(3f, 12f);
     }
 }
