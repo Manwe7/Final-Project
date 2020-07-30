ebff25b87724a75a1ddd1bdfd25f8e1e995b4faa
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-30 23:25:18 +0500

    Base class for loading record

---
+++ Assets/Scripts/Base classes/BaseIntRepo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class BaseIntRepo : IRepo<int>
+{
+    protected abstract string _key { get; }
+    
+    public void Save(int value)
+    {
+        PlayerPrefs.SetInt(_key, value);
+    }
+
+    public int Get()
+    {
+        return PlayerPrefs.GetInt(_key, 0);
+    }
+}
