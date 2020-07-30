86c7ddd13739c73ce12e927dac770e1064faae3c
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-30 23:32:28 +0500

    SOLID implementation

--- Assets/Scripts/Player/FuelHandler.cs
+++ Assets/Scripts/Player/FuelHandler.cs
@@ -1,35 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelHandler : MonoBehaviour
 {
     [SerializeField] private Slider _playerFuelSlider;
     
-    [SerializeField] private PlayerFlying _playerFlying;
+    private float _maxfuelCapacity = 30;
+    
+    private float _fuelCapacity;
 
-    [HideInInspector] public float _maxfuelCapacity = 50;
+    private bool _shouldDelayFuelRestoring;
 
-    [HideInInspector] public float _fuelCapacity;
+    private bool isFlying;
+    
+    
+    private bool _isRestoringFuel => _fuelCapacity < _maxfuelCapacity && !_shouldDelayFuelRestoring;
 
-    private void Awake()
-    {
-        _playerFlying.OnFuelCapacityChange += SliderValue;   
-    }
+    private bool _canRestoreFuel => _fuelCapacity <= 0 && !_shouldDelayFuelRestoring;
+
+    public bool HasFule => _fuelCapacity > 0;
+    
 
     private void Start()
     {        
         _fuelCapacity = _maxfuelCapacity;
         _playerFuelSlider.maxValue = _maxfuelCapacity;
-        SliderValue(_fuelCapacity);
     }
 
-    private void OnDestroy()
+    private void Update()
     {
-        _playerFlying.OnFuelCapacityChange -= SliderValue;
+        if (_canRestoreFuel)
+        {
+            StartCoroutine(DelayRestoringFuel());
+        }
+
+        SliderValue(_fuelCapacity);
     }
 
     private void SliderValue(float capacity)
     {
         _playerFuelSlider.value = _fuelCapacity;
     }
+
+    private IEnumerator DelayRestoringFuel()
+    {
+        _shouldDelayFuelRestoring = true;
+        yield return new WaitForSeconds(1f);
+        _shouldDelayFuelRestoring = false;
+    }
+
+    public void RestoreFuel()
+    {
+        if(_isRestoringFuel)
+        {
+            _fuelCapacity += 0.1f;
+        }
+    }
+
+    public void SpendFuel()
+    {
+        _fuelCapacity -= 0.1f;
+    }    
 }
