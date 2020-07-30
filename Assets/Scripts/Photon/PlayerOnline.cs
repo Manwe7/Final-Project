58dd8afe541ca235f05c9e0a423fe45feed7355b
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-31 01:36:21 +0500

    SOLID implementation

--- Assets/Scripts/Photon/PlayerOnline.cs
+++ Assets/Scripts/Photon/PlayerOnline.cs
@@ -14,27 +14,34 @@ public class PlayerOnline : MonoBehaviour, IPunObservable
     
     private Slider _healthSlider;
 
-    private float _remainingLives;
+    private int _remainingLives;
 
     private GameObject _endPanel;
 
     private Text _endPanelText;
 
-    private Text _livesAmountText;
-
-
-    //Components to DeActivate
+    [Header("Components to DeActivate")]
     [SerializeField] private GameObject weapon;
-    [SerializeField] private GameObject fuelParticles;
-    private PlayerMovementOnline _playerMovementOnline;
-    private SpriteRenderer _spriteRenderer;
-    private BoxCollider2D _boxCollider2D;
-    private Rigidbody2D _rigidbody2D;
 
+    [SerializeField] private GameObject fuelParticles;
+
+    [SerializeField] private PlayerMovementOnline _playerMovementOnline;
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private BoxCollider2D _boxCollider2D;
+
+    [SerializeField] private Rigidbody2D _rigidbody2D;
 
     private Vector2 deathPosition;
+
     private bool respawned, killed;
+
     private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+    private ExitGames.Client.Photon.Hashtable _myCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+    private Text[] _playerScore = new Text[2];
 
     private void Awake()
     {
@@ -44,19 +51,8 @@ public class PlayerOnline : MonoBehaviour, IPunObservable
         _endPanel = GameObject.Find("Canvas/EndPanel");
         _endPanelText = GameObject.Find("Canvas/EndPanel/Text").GetComponent<Text>();
 
-        if(PhotonNetwork.IsMasterClient)
-        {
-            _livesAmountText = GameObject.Find("CanvasPlayer0Lives").GetComponent<Text>();
-        }
-        else
-        {
-            _livesAmountText = GameObject.Find("CanvasPlayer1Lives").GetComponent<Text>();
-        }
-
-        _playerMovementOnline = GetComponent<PlayerMovementOnline>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _boxCollider2D = GetComponent<BoxCollider2D>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerScore[0] = GameObject.Find("Canvas/Player0Lives").GetComponent<Text>();
+        _playerScore[1] = GameObject.Find("Canvas/Player1Lives").GetComponent<Text>();
 
         _cinemachineVirtualCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInChildren<CinemachineVirtualCamera>();        
     }
@@ -70,13 +66,28 @@ public class PlayerOnline : MonoBehaviour, IPunObservable
         _health = 100;
         _healthSlider.maxValue = _health;
 
-        _remainingLives = 3;
-        _livesAmountText.text = _remainingLives.ToString();
+        SetLives(3);
 
         killed = false;
 
         _cinemachineVirtualCamera.Follow = gameObject.transform;
         _cinemachineVirtualCamera.LookAt = gameObject.transform;
+    }
+
+    private void SetLives(int value)
+    {
+        _remainingLives = value;
+        _myCustomProperties[ShowScoreOnline.healthSave] = _remainingLives;        
+        PhotonNetwork.SetPlayerCustomProperties(_myCustomProperties);
+
+        // for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        // {
+        //     if(PhotonNetwork.PlayerList[i].CustomProperties[ShowScoreOnline.healthSave] != null)
+        //     {
+        //         int remainingLive = (int)PhotonNetwork.PlayerList[i].CustomProperties[ShowScoreOnline.healthSave];
+        //         _playerScore[i].text = remainingLive.ToString();
+        //     }                        
+        // }
     }
 
     private void Update()
@@ -128,14 +139,17 @@ public class PlayerOnline : MonoBehaviour, IPunObservable
     [PunRPC]
     public void Killed()
     {
+        if(!_photonView.IsMine) { return; }
+
         if(killed) { return; }
-                   
+
         PhotonNetwork.Instantiate(_playerExplosion.name, transform.position, Quaternion.identity);
 
         _remainingLives -= 1;
-        _livesAmountText.text = _remainingLives.ToString();
+        SetLives(_remainingLives);
+
         killed = true;
-        deathPosition = gameObject.transform.position;        
+        deathPosition = gameObject.transform.position;
 
         weapon.SetActive(false);
         fuelParticles.SetActive(false);
@@ -185,7 +199,7 @@ public class PlayerOnline : MonoBehaviour, IPunObservable
             _rigidbody2D.gravityScale = 0;
             _playerMovementOnline.enabled = true;
 
-            _health = 100;            
+            _health = 100;
 
             respawned = true;
         }
