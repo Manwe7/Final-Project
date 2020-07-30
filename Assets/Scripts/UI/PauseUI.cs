6df40f998234e18d54d88b9545f40299d17c044a
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-30 23:32:48 +0500

    SOLID implementation

--- Assets/Scripts/UI/PauseUI.cs
+++ Assets/Scripts/UI/PauseUI.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PauseUI : MonoBehaviour
@@ -6,7 +7,35 @@ public class PauseUI : MonoBehaviour
     [Header("Panels")]
     [SerializeField] private GameObject _pauseMenuPanel = null;
 
-    [SerializeField] private GameSpeed _gameSpeed; 
+    [Header("Scripts")]
+    [SerializeField] private GameSpeed _gameSpeed;
+
+    [SerializeField] private GamePlayUI _gamePlayUI;
+
+    [Header("Record on pause panel")]
+    [SerializeField] private Text _pauseRecordText;
+
+    private int _record;
+
+    private IGetRepo<int> _repo;
+
+    private void Awake()
+    {
+        _repo = new RecordRepo();
+        
+        _gamePlayUI.OnGamePause += ShowRecord;
+    }
+
+    private void OnDestroy()
+    {
+        _gamePlayUI.OnGamePause -= ShowRecord;
+    }
+
+    private void ShowRecord()
+    {
+        _record = _repo.Get();
+        _pauseRecordText.text = _record.ToString();
+    }
 
     public void Resume()
     {
