45afd43df45aca4c75d01df4729084dbe9ca4287
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-31 01:36:35 +0500

    SOLID implementation

--- Assets/Scripts/Photon/ShowScoreOnline.cs
+++ Assets/Scripts/Photon/ShowScoreOnline.cs
@@ -1,20 +1,42 @@
 ﻿using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using Photon.Realtime;
 
 public class ShowScoreOnline : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Text[] _playerName;
 
-    [SerializeField] private Text _player0Score;
+    [SerializeField] private Text[] _playerScore;
 
-    [SerializeField] private Text _player1Score;
+    public Player _player { get; private set; }
 
-    public void Start()
+    public const string healthSave = "RemainingHealth";
+
+    public void Update()
+    {
+        SetPlayerText();
+    }
+
+    // public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    // {
+    //     if(targetPlayer != null)
+    //     {
+    //         SetPlayerText();
+    //     }
+    // }
+
+    private void SetPlayerText()
     {
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
             _playerName[i].text = PhotonNetwork.PlayerList[i].NickName;
-        }        
-    }
+
+            if(PhotonNetwork.PlayerList[i].CustomProperties[healthSave] != null)
+            {
+                int remainingLive = (int)PhotonNetwork.PlayerList[i].CustomProperties[healthSave];
+                _playerScore[i].text = remainingLive.ToString();
+            }
+        }
+    }   
 }
