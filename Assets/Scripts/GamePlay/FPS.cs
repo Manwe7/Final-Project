6d973e88ee98f94b467d1f0f99f9afe31c958871
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-30 23:31:44 +0500

    Set target frame rate

---
+++ Assets/Scripts/GamePlay/FPS.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    [SerializeField] private Text _fpsText = null;
+    private float deltaTime;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        ShowFPS();
+    }
+
+    private void ShowFPS()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        _fpsText.text = Mathf.Ceil(fps).ToString();
+    }
+}
