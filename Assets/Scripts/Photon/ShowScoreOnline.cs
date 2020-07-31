374f5652cec81de129dcefbeef8ed0d9a7b192d1
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-07-31 17:36:45 +0500

    Show NickNames and player Lives

--- Assets/Scripts/Photon/ShowScoreOnline.cs
+++ Assets/Scripts/Photon/ShowScoreOnline.cs
@@ -9,22 +9,16 @@ public class ShowScoreOnline : MonoBehaviourPunCallbacks
 
     [SerializeField] private Text[] _playerScore;
 
-    public Player _player { get; private set; }
+    //public Player _player { get; private set; }
 
     public const string healthSave = "RemainingHealth";
+
+    private int _remainingLives;
 
     public void Update()
     {
         SetPlayerText();
     }
-
-    // public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
-    // {
-    //     if(targetPlayer != null)
-    //     {
-    //         SetPlayerText();
-    //     }
-    // }
 
     private void SetPlayerText()
     {
@@ -34,9 +28,9 @@ public class ShowScoreOnline : MonoBehaviourPunCallbacks
 
             if(PhotonNetwork.PlayerList[i].CustomProperties[healthSave] != null)
             {
-                int remainingLive = (int)PhotonNetwork.PlayerList[i].CustomProperties[healthSave];
-                _playerScore[i].text = remainingLive.ToString();
+                _remainingLives = (int)PhotonNetwork.PlayerList[i].CustomProperties[healthSave];
+                _playerScore[i].text = _remainingLives.ToString();                            
             }
-        }
-    }   
+        }        
+    }
 }
