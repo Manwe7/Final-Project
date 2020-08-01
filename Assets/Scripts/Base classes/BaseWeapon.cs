a67f9a0c127ba93fd05e3958f767f0e0d1e15947
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-01 23:02:12 +0500

    Bas class for enemy and player weapon classes

---
+++ Assets/Scripts/Base classes/BaseWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour
+{
+    [SerializeField] protected GameObject _bullet;
+
+    [SerializeField] protected Transform _barrel;
+
+    protected float _reloadTime;
+
+    protected bool _reloaded;
+
+    protected IEnumerator Reload()
+    {
+        _reloaded = false;
+        yield return new WaitForSeconds(_reloadTime);
+        _reloaded = true;
+    }
+}
