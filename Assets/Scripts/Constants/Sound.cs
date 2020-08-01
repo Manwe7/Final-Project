51d4d959e190dfd5f38994edc3850489f5f21ca2
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-01 23:06:02 +0500

    Change folder

---
+++ Assets/Scripts/Constants/Sound.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public SoundNames SoundName;
+
+    public AudioClip AudioClip;
+
+    [Range(0f, 1f)] public float Volume;
+
+    [Range(-3f, 3f)] public float Pitch;
+
+    public bool Loop;
+
+    [HideInInspector] public AudioSource Source;
+    
+    [Range (0f, 1f)] public float SpacialBlend;
+}
+
+public enum SoundNames
+    {
+        PlayerDeath,
+        EnemyDeath,
+        PlayerBullet,
+        Hurt
+    }
