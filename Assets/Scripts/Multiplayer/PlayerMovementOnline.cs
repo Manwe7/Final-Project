1c5a92dc65a1a577d7f18f44d4d2615dffc58df5
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-01 22:50:42 +0500

    Rename folder

---
+++ Assets/Scripts/Multiplayer/PlayerMovementOnline.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Photon.Pun;
+
+public class PlayerMovementOnline : MonoBehaviour
+{
+    [SerializeField] private PhotonView _photonView;
+
+    [SerializeField] private GameObject fuelParticles = null;
+
+    private FixedJoystick _fixedjoystick = null;
+    private Slider _fuelSlider = null;
+    private Rigidbody2D _rigidbody2D = null;
+
+    private float moveSpeed = 17;
+    private float maxfuelCapacity = 50;
+    private float _horizontalMove, _verticalMove;
+    private float _fuelCapacity;
+    private bool _reloadFuel;
+
+    private void Awake()
+    {
+        if (!_photonView.IsMine) { return; }
+
+        _fixedjoystick = GameObject.Find("Canvas/MovementJoystick").GetComponent<FixedJoystick>();
+        _fuelSlider = GameObject.Find("Canvas/PlayerFuelSlider").GetComponent<Slider>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        if (!_photonView.IsMine) { return; }
+
+        _reloadFuel = true;
+        fuelParticles.SetActive(false);
+
+        _fuelCapacity = maxfuelCapacity;
+        _fuelSlider.maxValue = maxfuelCapacity;
+    }
+    
+    private void Update()
+    {
+        if (!_photonView.IsMine) { return; }
+
+        _fuelSlider.value = _fuelCapacity;
+        _verticalMove = _fixedjoystick.Vertical;
+        _horizontalMove = _fixedjoystick.Horizontal;
+
+        if (_verticalMove > 0.22f && _fuelCapacity > 0)
+        {
+            fuelParticles.SetActive(true);
+            _fuelCapacity -= 0.1f;
+        }
+        else
+        {
+            fuelParticles.SetActive(false);
+            if (_fuelCapacity < maxfuelCapacity && _reloadFuel)
+            {
+                _fuelCapacity += 0.1f;
+            }
+        }
+
+        if (_fuelCapacity <= 0 && _reloadFuel == true)
+        { 
+            StartCoroutine(ReloadFuel());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_photonView.IsMine) { return; }
+
+        _rigidbody2D.velocity = new Vector2(_horizontalMove * moveSpeed, _rigidbody2D.velocity.y);
+
+        if (_verticalMove > 0.22f && _fuelCapacity > 0)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _verticalMove * 8);
+            _rigidbody2D.gravityScale = 0;
+        }
+        else
+        {
+            _rigidbody2D.gravityScale = 5;
+        }
+    }
+
+    private IEnumerator ReloadFuel()
+    {
+        _reloadFuel = false;
+        yield return new WaitForSeconds(2f);
+        _reloadFuel = true;
+    }
+}
---
+++ Assets/Scripts/Multiplayer/PlayerWeaponOnline.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerWeaponOnline : MonoBehaviour
+{
+    [SerializeField] private PhotonView _photonView;
+
+    [SerializeField] private Transform parent = null, barrel = null;
+
+    [SerializeField] private GameObject BulletOnline = null;
+    
+    [SerializeField] private float reloadTime = 0;
+
+    private Joystick _fixedjoystick = null;
+    private RectTransform _joystickHandle = null;    
+    private float _offset = 180;
+
+    private bool _reloaded;
+
+    private void Awake()
+    {        
+        if (!_photonView.IsMine) { return; }
+
+        _fixedjoystick = GameObject.Find("Canvas/RotationJoystick").GetComponent<FixedJoystick>();
+        _joystickHandle = GameObject.Find("Canvas/RotationJoystick/Handle").GetComponent<RectTransform>();        
+    }
+
+    private void Start()
+    {
+        _reloaded = true;
+    }
+
+    private void Update()
+    {
+        if (!_photonView.IsMine) { return; }
+
+        if (_joystickHandle.anchoredPosition.x < 0 && _joystickHandle.anchoredPosition.x != 0)//(mouse.x < playerScreenPoint.x) 
+        {
+            LeftSide();
+        }
+        else if (_joystickHandle.anchoredPosition.x > 0)
+        {
+            RightSide();
+        }
+
+        if (_reloaded)
+        {
+            Shoot();
+            _reloaded = false;
+            Invoke("Reload", reloadTime);
+        }
+
+    }
+    
+    [PunRPC]
+    public void Shoot()
+    {
+        GameObject bullet = PhotonNetwork.Instantiate(BulletOnline.name, barrel.position, barrel.rotation);
+        bullet.name = parent.gameObject.name + "Bullet";
+    }
+
+    private void Reload()
+    {
+        _reloaded = true;
+    }
+
+    private void RightSide()
+    {
+        Vector3 direction = Vector3.up * _fixedjoystick.Vertical + Vector3.right * _fixedjoystick.Horizontal;
+        float rotZ = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        if (rotZ != 0)
+        { transform.rotation = Quaternion.Euler(0f, 0f, -rotZ); }
+
+        transform.position = new Vector3(parent.transform.position.x + 0.3f, parent.transform.position.y, parent.transform.position.z);
+    }
+
+    private void LeftSide()
+    {
+        Vector3 direction = Vector3.up * _fixedjoystick.Vertical + Vector3.right * _fixedjoystick.Horizontal;
+        float rotZ = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        if (rotZ != 0)
+        { transform.rotation = Quaternion.Euler(-180f, 0f, rotZ + _offset); }
+
+        transform.position = new Vector3(parent.transform.position.x - 0.3f, parent.transform.transform.position.y, parent.transform.position.z);
+    }
+}
