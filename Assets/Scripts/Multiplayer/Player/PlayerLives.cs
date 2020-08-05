179405a07cedf851e89ac0d564b54b6047e620c8
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-06 00:07:19 +0500

    Hadle player lives saving

---
+++ Assets/Scripts/Multiplayer/Player/PlayerLives.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace PlayerOfflineScipts
+{
+    public class PlayerLives : MonoBehaviour
+    {
+        [SerializeField] private PlayerOnlineScripts.Player _player;
+
+        private ExitGames.Client.Photon.Hashtable _myCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+        private void Start()
+        {
+            SetLives(3);
+        }
+
+        private void OnEnable()
+        {
+            _player.OnLivesChange += SetLives;
+        }
+
+        private void OnDisable()
+        {
+            _player.OnLivesChange -= SetLives;
+        }
+
+        private void SetLives(int value)
+        {
+            //_remainingLives = value;
+            _myCustomProperties[ShowScoreOnline.healthSaveKey] = value; //_remainingLives;
+            PhotonNetwork.SetPlayerCustomProperties(_myCustomProperties);
+        }
+
+    }
+}
