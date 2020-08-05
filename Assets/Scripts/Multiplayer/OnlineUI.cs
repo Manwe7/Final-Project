2195b15cc34f3d6bc64788a2ed5cc2dbd602cf5c
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-06 00:06:04 +0500

    To handle UI

---
+++ Assets/Scripts/Multiplayer/OnlineUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class OnlineUI : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _pausePanel;
+
+    public void PauseGame()
+    {
+        _pausePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        _pausePanel.SetActive(false);
+    }
+
+    public void LeaveRoom()
+    { 
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
