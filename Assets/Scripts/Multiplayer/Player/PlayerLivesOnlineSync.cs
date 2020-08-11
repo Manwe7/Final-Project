8eda2a9deddbbf074112e30ce129b06c9b5cf96a
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-11 21:49:43 +0500

    Code refactoring

---
+++ Assets/Scripts/Multiplayer/Player/PlayerLivesOnlineSync.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace PlayerOnlineScripts
+{
+    public class PlayerLivesOnlineSync : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+
+        private ExitGames.Client.Photon.Hashtable _myCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+        private int _remainingLives;
+
+        private void Start()
+        {
+            _remainingLives = 3;
+            SetLives(_remainingLives);
+        }
+
+        private void SetLives(int value)
+        {
+            _myCustomProperties[ShowScoreOnline.LivesSaveKey] = value;
+            PhotonNetwork.SetPlayerCustomProperties(_myCustomProperties);
+        }
+
+        public void DecreaseOneLife()
+        {
+            _remainingLives -= 1;
+            SetLives(_remainingLives);
+        }
+
+        public bool IsEnoughLives()
+        {
+            return _remainingLives > 0;
+        }
+    }
+}
