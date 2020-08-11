c5d6cdbdd6301fd7b0922f0391d7a3b88cf2165b
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-11 21:48:38 +0500

    Code refactoring

---
+++ Assets/Scripts/Base classes/BaseSaveClassRepo.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+
+[Serializable]
+public class SaveAttributes
+{
+    public int Money;
+
+    public int Record;
+}
+
+public abstract class BaseSaveClassRepo : IRepo<SaveAttributes>
+{
+    public void Save(SaveAttributes value)
+    {
+        string json = JsonUtility.ToJson(value);
+        File.WriteAllText(Application.dataPath + "/save.txt", json);
+    }
+
+    public SaveAttributes Get()
+    {
+        if(File.Exists(Application.dataPath + "/save.txt"))
+        {
+            string savedString = File.ReadAllText(Application.dataPath + "/save.txt");
+            SaveAttributes myObject = JsonUtility.FromJson<SaveAttributes>(savedString);        
+
+            return myObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    // public void Save(int value)
+    // {
+    //     PlayerPrefs.SetInt(_key, value);
+    // }
+
+    // public int Get()
+    // {
+    //     return PlayerPrefs.GetInt(_key, 0);
+    // }
+}
