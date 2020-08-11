cd411e6cdac2de41fac7153db09ef64810b4de8d
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-11 21:49:15 +0500

    Code refactoring

---
+++ Assets/Scripts/Multiplayer/Player/EndGamePanel.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using System.Collections;
+
+namespace PlayerOnlineScripts
+{
+    public class EndGamePanel : MonoBehaviourPun
+    {
+        [SerializeField] private PlayerLivesOnlineSync _playerLivesOnlineSync;
+
+        [SerializeField] private PhotonView _photonView;
+
+        private GameObject _endPanel;
+
+        private Text _endPanelText;
+
+        private void Awake()
+        {
+            if (!_photonView.IsMine) { return; }
+
+            FindUIObjects();
+        }
+
+        private void FindUIObjects()
+        {
+            _endPanel = GameObject.Find("Canvas/EndPanel");
+            _endPanelText = GameObject.Find("Canvas/EndPanel/Text").GetComponent<Text>();
+        }
+
+        private void Start()
+        {
+            _endPanel.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (!_photonView.IsMine) { return; }
+
+            OpenEndGamePanel();
+        }
+
+        private void OpenEndGamePanel()
+        {
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            {
+                if(PhotonNetwork.PlayerList[i].CustomProperties[ShowScoreOnline.LivesSaveKey] != null)
+                {
+                    int lives = (int)PhotonNetwork.PlayerList[i].CustomProperties[ShowScoreOnline.LivesSaveKey];
+                    if(lives <= 0)
+                    {
+                        StartCoroutine(StopGame());
+                    }
+                }
+            }        
+        }
+
+        private IEnumerator StopGame()
+        {
+            yield return new WaitForSeconds(1.7f);
+            Time.timeScale = 0.1f;
+            _endPanel.SetActive(true);
+            
+            if(_playerLivesOnlineSync.IsEnoughLives())
+            {
+                _endPanelText.text = "VICTORY";
+            }
+            else
+            {
+                _endPanelText.text = "DEFEAT";
+            }
+        }
+    }
+}
