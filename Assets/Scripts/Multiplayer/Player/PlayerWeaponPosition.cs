dab7fa9ec17e16abbb7a228faa41682e4ea6912f
Manwe7 <43933209+Manwe7@users.noreply.github.com>
2020-08-12 14:01:30 +0500

    Code refactoring

--- Assets/Scripts/Multiplayer/Player/PlayerWeaponPosition.cs
+++ Assets/Scripts/Multiplayer/Player/PlayerWeaponPosition.cs
@@ -3,13 +3,9 @@ using UnityEngine;
 
 namespace PlayerOnlineScripts
 {
-    public class PlayerWeaponPosition : BaseWeaponPosition
+    public class PlayerWeaponPosition : PlayerOfflineScipts.PlayerWeaponPosition
     {
         [SerializeField] private PhotonView _photonView;
-        
-        private Joystick _weaponJoystick;
-
-        private RectTransform _joystickHandle;
 
         private void Awake()
         {        
@@ -19,40 +15,12 @@ namespace PlayerOnlineScripts
             _joystickHandle = GameObject.Find("Canvas/RotationJoystick/Handle").GetComponent<RectTransform>();
         }
 
-        private void Start()
-        {
-            _offset = 180;
-        }
-
         protected override void ChangeWeaponPosition()
         {
             if (!_photonView.IsMine) { return; }
             
-            _direction = Vector3.up * _weaponJoystick.Vertical + Vector3.right * _weaponJoystick.Horizontal;
-            _rotationZ = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
-
-            if(_rotationZ == 0) return;
-
-            if (_joystickHandle.anchoredPosition.x < 0)
-            {
-                SetToLeftSide();
-            }
-            else if (_joystickHandle.anchoredPosition.x > 0)
-            {
-                SetToRightSide();
-            }
+            base.ChangeWeaponPosition();
         }
 
-        private void SetToRightSide()
-        {       
-            transform.rotation = Quaternion.Euler(0f, 0f, -_rotationZ);
-            transform.position = GetPosition(0.3f);
-        }
-
-        private void SetToLeftSide()
-        {
-            transform.rotation = Quaternion.Euler(-180f, 0f, _rotationZ + _offset);
-            transform.position = GetPosition(-0.3f);
-        }
     }
 }
